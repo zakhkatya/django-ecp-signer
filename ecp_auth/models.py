b4dbb2e6a190ec0b31fc3c733bb28f54db7a0a31
Bachok Viktor <bachok.viktor@gmail.com>
2026-03-26 13:29:20 +0200

    Implement nonce and certificate models.

---
+++ ecp_auth/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils import timezone
+import secrets
+
+# Value in minutes
+NONCE_LIFETIME = 5
+
+
+def gen_secret_token():
+    return secrets.token_hex(32)
+
+
+class ECPNonce(models.Model):
+    """ This model represents ecp nonce.
+    """
+    value = models.CharField(
+        max_length=64,
+        default=gen_secret_token,
+        unique=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    used = models.BooleanField(default=False)
+
+    def is_valid(self):
+        nonce_age = timezone.now() - self.created_at
+
+        return (not self.used) and (
+            int(nonce_age.total_seconds()/60) < NONCE_LIFETIME
+        )
+
+    def consume(self):
+        self.used = True
+
+
+class ECPCertificate(models.Model):
+    """ This model represents ecp certificate.
+    """
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    taxpayer_id = models.CharField(max_length=256, unique=True)
+    certificate_pem = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
